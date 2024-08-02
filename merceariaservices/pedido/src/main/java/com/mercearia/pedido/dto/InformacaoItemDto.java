a883fbaf916912fc564869c1609b4378680a0333
DanielLucena <daniel.lucena.707@ufrn.edu.br>
2024-08-02 15:58:35 +0000

    feat: adiciona dtos do serviço de Pedido

---
+++ merceariaservices/pedido/src/main/java/com/mercearia/pedido/dto/InformacaoItemDto.java
@@ -0,0 +1,17 @@
+package com.mercearia.pedido.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InformacaoItemDto {
+    private Integer codigoProduto;
+    private String nome;
+    private int quantidade;
+    private double preco;
+}
---
+++ merceariaservices/pedido/src/main/java/com/mercearia/pedido/dto/ItemDto.java
@@ -0,0 +1,15 @@
+package com.mercearia.pedido.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ItemDto {
+    private Integer produto;
+    private Integer quantidade;
+}
