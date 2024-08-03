ef78dea5641adac1d3c2ca53d50f74424f2044e5
Daniel Lucena <drlucena@gmail.com>
2024-08-03 16:15:41 +0000

    feat: adiciona serviço do config server ⚙️

---
+++ merceariaservices/config-server/src/test/java/com/mercearia/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercearia.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
